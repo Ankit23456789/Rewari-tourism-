7a06b2b0e9796ff01297980e2e143ed320ae98e4
Ankit yadav <ankityadav281998@gmail.com>
2024-01-17 20:34:29 +0530

    first

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    nav: true,
+    center:true,
+    navText:["<",">"],
+    slideBy: 2,
+   
+    responsive: {
+        0: {
+            items: 1,
+            nav:false
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+            
+        }
+    }
+})
+
+AOS.init();
