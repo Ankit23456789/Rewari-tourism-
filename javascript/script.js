e6427933125837d4352c3b5254daae420323b94b
Ankit yadav <ankityadav281998@gmail.com>
2024-01-22 12:57:05 +0530

    new commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,18 @@
+// Navbar script start
+const hamburger =document.querySelector(".hamburger");
+const navMenu =document.querySelector(".nav-menu");
+
+hamburger .addEventListener("click",()=>{
+     hamburger.classList.toggle("active");
+     navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click",()=>{
+        hamburger.classList.remove("active")
+        navMenu.classList.remove("active")
+ 
+}))
+// Navbar script End
 $('.owl-carousel').owlCarousel({
     loop: true,
     responsiveClass: true,
